2958f687b7a5d9881696445cc0db533ca046106b
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-03-16 15:49:30 -0600

    reducer

--- src/dog/reducer.ts
+++ src/dog/reducer.ts
@@ -1,8 +1,7 @@
 import { getType } from 'typesafe-actions';
-
+import { combineReducers } from 'redux';
 import { RootAction } from '../rootAction';
 import { DogActions } from './actions';
-
 
 /* === State === */
 export type DogState = {
@@ -19,23 +18,50 @@ export const InitialDogState: DogState = {
 
 
 /* === REDUCER === */
-export function DogReducer(state: DogState = InitialDogState, action: RootAction) {
-  switch(action.type) {
-    case getType(DogActions.request):
-      return {
-        ...state,
-        fetching: !state.fetching
-      }
-    case getType(DogActions.failure):
-      return {
-        ...state,
-        error: action.payload
-      }
-    case getType(DogActions.success):
-      return {
-        ...state,
-        imgSrc: action.payload
-      }
-    default: return state;
+// export function DogReducer(state: DogState = InitialDogState, action: RootAction) {
+//   switch(action.type) {
+//     case getType(DogActions.request):
+//       return {
+//         ...state,
+//         fetching: !state.fetching
+//       }
+//     case getType(DogActions.failure):
+//       return {
+//         ...state,
+//         error: action.payload
+//       }
+//     case getType(DogActions.success):
+//       return {
+//         ...state,
+//         imgSrc: action.payload
+//       }
+//     default: return state;
+//   }
+// };
+
+export const DogReducer = combineReducers<DogActions>({
+  fetching: (state = false, action) => {
+    switch(action.type) {
+      case getType(DogActions.request):
+        return true;
+      case getType(DogActions.success):
+      case getType(DogActions.failure):
+        return false;
+      default: return state;
+    }
+  },
+  error: (state = null, action) => {
+    switch(action.type) {
+      case getType(DogActions.failure):
+        return action.payload;
+      default: return state;
+    }
+  },
+  imgSrc: (state = null, action) => {
+    switch(action.type) {
+      case getType(DogActions.success):
+        return action.payload;
+      default: return state;
+    }
   }
-};
+})
