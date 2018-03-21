23e5e592f90d4df6f72e8617f6c0d78b1958a638
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-03-21 12:29:00 -0600

    fix?

--- src/graph/epics/nodes.ts
+++ src/graph/epics/nodes.ts
@@ -13,7 +13,7 @@ import { GraphNode, UpdateGraphNode } from '../types';
 
 const createNodeEpic: Epic<RootAction, RootState> = (action$, store) => action$
   .filter(isActionOf(GraphActions.requestAddNode))
-  .do(() => { console.log('in node epic'); })
+  .do(() => { console.log('test create epic'); })
   .debounceTime(400)
   .flatMap(() => {
     return Observable
@@ -27,7 +27,7 @@ const deleteNodeEpic: Epic<RootAction, RootState> = (action$, store) => action$
   .filter(isActionOf(GraphActions.requestRemoveNode))
   .do((test) => { console.log('delete node epic', test) })
   .debounceTime(400)
-  .map(action => GraphActions.successRemoveNode({ ...action.payload } as GraphNode))
+  .map(action => GraphActions.successRemoveNode({ ...action.payload} as GraphNode))
   .catch(error => Observable.of(GraphActions.failureRemoveNode(error)))
 
 const updateNodeEpic: Epic<RootAction, RootState> = (action$, store) => action$
