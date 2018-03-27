26357894aacbf11e8ab443ba0148d97a7075dcfb
colton <colton.morris@tcnbroadcasting.com>
2018-03-26 18:31:51 -0600

    comment on magic rootAction

--- src/rootAction.ts
+++ src/rootAction.ts
@@ -9,5 +9,6 @@ const returnsOfActions = {
   ..._.map(_.values(DogActions), $call),
   ..._.map(_.values(GraphActions), $call),
 }
+// This implicity maps over the whole returnsOfActions object where the key is of type number. This is basically a fancy loop that 'OR's all of the actionTypes.
 export type RootAction = typeof returnsOfActions[number];
 
